a04eda1e5c69ffe135781a4446574ec52f45258b
okunolamichael <59531059+okunolamichael@users.noreply.github.com>
2024-09-11 13:56:32 +0100

    testing commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -21,7 +21,9 @@ const Hero = () => {
                 <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/>
             </div>
 
-            <div className="flex justify-center"></div>
+            <div className="flex justify-center">
+                <div className="max-w-[80vw]"></div>
+            </div>
         </div>
     );
 };
